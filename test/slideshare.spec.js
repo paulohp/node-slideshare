fea814962ca8fc47b023b42d596483b1aaf303e8
Paulo Pires <taralloqueiroiz@gmail.com>
2015-07-15 17:24:24 -0700

    added test folder, mocha and chai

---
+++ test/slideshare.spec.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+import Slideshare from '../src/slideshare';
+
+describe('Slideshare', () => {
+  let s;
+  beforeEach(() => {
+    s = new Slideshare('', '');
+  });
+});
